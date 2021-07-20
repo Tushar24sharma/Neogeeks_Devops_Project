e3bd122c965338e04060de4246bfa1f449eea22d
Tushar24sharma <85062785+Tushar24sharma@users.noreply.github.com>
2021-07-21 00:40:39 +0530

    Create while.py

---
+++ while.py
@@ -0,0 +1,18 @@
+import time
+
+while 7>6:
+    print("As engineers, we were going to be in a position to change the world – not just study it.")
+    time.sleep(5)
+    print("This job is a great scientific adventure. But it’s also a great human adventure.")
+    time.sleep(5)
+    print("The scientist discovers a new type of material or energy and the engineer discovers a new use for it.")
+    time.sleep(5)
+    print("Science can amuse and fascinate us all, but it is engineering that changes the world.")
+    time.sleep(5)
+    print("The engineer has been, and is, a maker of history.")
+    time.sleep(5)
+    print("Scientists study the world as it is; engineers create the world that has never been.")
+    time.sleep(5)
+    print("The way to succeed is to double your failure rate.")
+    time.sleep(5)
+    print("Strive for perfection in everything you do. Take the best that exists and make it better. When it does not exist, design it.")
